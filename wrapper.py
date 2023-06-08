31eacba8f67a42dfe2df5e89dacc3aefac6cd2ba
Nerses Tamazyan <nerses.tamazyan@goya.am>
2023-06-08 10:44:26 +0400

    Made some changes in the wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -1,19 +1,25 @@
-import yaml
 import os
+import yaml
 
 
 def get_telegram_bot_configs():
     try:
-        # Read the config file
-        with open('config.yml', 'r') as file:
-            config = yaml.safe_load(file)
+        # Check if running on AWS environment
+        if os.environ.get('ENVIRONMENT') == 'aws':
+            api_key = os.environ['API_KEY']
+            bot_name = os.environ['BOT_NAME']
+            openai_key = os.environ['OPENAI_KEY']
+        else:
+            # Read the config file
+            with open('config.yml', 'r') as file:
+                config = yaml.safe_load(file)
 
-        # Access the values
-        api_key = config['api_key']
-        bot_name = config['bot_name']
-        openai_key = config['openai_key']
-    except (yaml.YAMLError, FileNotFoundError, KeyError):
-        api_key = os.environ['API_KEY']
-        bot_name = os.environ['BOT_NAME']
-        openai_key = os.environ['OPENAI_KEY']
+            # Access the values
+            api_key = config['api_key']
+            bot_name = config['bot_name']
+            openai_key = config['openai_key']
+    except (yaml.YAMLError, FileNotFoundError, KeyError, TypeError):
+        # Handle the case if any of the values are missing
+        raise Exception('Bot token is not defined')
+
     return api_key, bot_name, openai_key
